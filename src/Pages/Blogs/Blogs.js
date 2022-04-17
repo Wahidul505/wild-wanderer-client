422627d9a0742df282b03101ae2c3a87ea6d601a
Wahidul30 <wahidul505@gmail.com>
2022-04-18 00:46:37 +0600

    commiting before taking a nap

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            
+            <h1>Difference Between Authentication and Authorization?</h1>
+            <p>• Authentication is about validating someones credential to verify identity. Authentication </p>
         </div>
     );
 };
