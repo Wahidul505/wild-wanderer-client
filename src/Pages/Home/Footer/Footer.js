3dc3816c99cdc95235fb5c84620e38dc51709d8d
Wahidul30 <wahidul505@gmail.com>
2022-04-17 21:13:08 +0600

    styled footer section

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { GrFacebookOption } from 'react-icons/gr';
+import { FaPinterestP } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
-        <div className='text-center mt-20 bg-stone-900 text-white py-4'>
-            <div className='text-center order-1 md:order-2'>
+        <div className='text-center mt-20 bg-black text-gray-50 py-4'>
+            <div className='text-center mb-12'>
                 <h1 className='text-3xl font-serif'>Wild</h1>
                 <h2 className='text-xl'>Wanderer</h2>
             </div>
-            <div className='flex justify-center gap-6'>
-
+            <div className='flex justify-center gap-10 text-sm mb-10'>
+                <Link to='/home'>HOME</Link>
+                <Link to='/about'>ABOUT ME</Link>
+                <Link to='/blogs'>BLOGS</Link>
             </div>
+            <div className='flex justify-center gap-10 text-base mb-10'>
+                <a href="https://www.instagram.com/wildlifeplanet/?hl=en"><FiInstagram /></a>
+                <a href="https://www.facebook.com/TheWCS/"><GrFacebookOption /></a>
+                <a href="https://www.pinterest.com/nickcobb/wildlife-photography/"><FaPinterestP /></a>
+            </div>
+            <p className='text-center opacity-60'><small>Copyright Collected © {year}</small></p>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -23,27 +23,27 @@ const Header = () => {
         <nav className={`flex justify-between items-center px-2 md:px-6 py-1 md:py-2 relative mb-16 
         ${isHome ? 'text-white' : 'text-black'}
         `}>
-            <div className='flex flex-col md:flex-row gap-0 md:gap-6 lg:gap-8 text-base  md:text-xl order-2 md:order-1'>
-                <NavLink to='/home'>Home</NavLink>
-                <NavLink to='/checkout'>CheckOut</NavLink>
-                <NavLink to='/blogs'>Blogs</NavLink>
+            <div className='flex flex-col md:flex-row gap-0 md:gap-6 lg:gap-8 text-base  md:text-lg order-2 md:order-1'>
+                <NavLink to='/home'>HOME</NavLink>
+                <NavLink to='/about'>ABOUT ME</NavLink>
+                <NavLink to='/blogs'>BLOGS</NavLink>
             </div>
             <div className='text-center order-1 md:order-2'>
                 <h1 className='text-2xl md:text-3xl font-serif'>Wild</h1>
                 <h2 className='text-lg md:text-xl'>Wanderer</h2>
             </div>
-            <div className='flex flex-col md:flex-row gap-0 md:gap-6 lg:gap-8 text-base md:text-xl order-3 md:order-3'>
-                <NavLink to='/about'>About</NavLink>
+            <div className='flex flex-col md:flex-row gap-0 md:gap-6 lg:gap-8 text-base md:text-lg order-3 md:order-3'>
+                <NavLink to='/checkout'>CHECK OUT</NavLink>
                 {
                     user ?
                         <button onClick={handleLogout} className={` md:py-1 md:px-2 rounded md:text-base text-center
-                        ${isHome ? 'bg-white text-black' : 'bg-gray-500 text-white'}`}>LogOut</button>
+                        ${isHome ? 'bg-white text-black' : 'bg-gray-500 text-white'}`}>LOG OUT</button>
                         :
                         <>
-                            <NavLink to='/signup'>SignUp</NavLink>
+                            <NavLink to='/signup'>SIGN UP</NavLink>
                             <NavLink to='/login'
                                 className={` md:py-1 md:px-2 rounded md:text-base text-center
-                                 ${isHome ? 'bg-white text-black' : 'bg-gray-500 text-white'}`}>LogIn</NavLink>
+                                 ${isHome ? 'bg-white text-black' : 'bg-gray-500 text-white'}`}>LOG IN</NavLink>
                         </>
                 }
             </div>
