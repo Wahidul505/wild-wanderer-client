e441d5ffba46a38b154b57ce4cf98149c43de621
Wahid <macbookair@MacBooks-MacBook-Air.local>
2023-04-30 14:11:45 +0600

    updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyC732C72qYG9np-LorQoN6K7d0VnyE-aTU",
+    authDomain: "wild-wanderer-b161c.firebaseapp.com",
+    projectId: "wild-wanderer-b161c",
+    storageBucket: "wild-wanderer-b161c.appspot.com",
+    messagingSenderId: "750003418962",
+    appId: "1:750003418962:web:c0219de0528f4e5080ec8b",
 };
 
 const app = initializeApp(firebaseConfig);
